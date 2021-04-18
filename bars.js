aeae243f43b54048571db4ab9bd78098633ec618
phildecroos <66972499+phildecroos@users.noreply.github.com>
2021-04-18 01:19:55 -0400

    Add files via upload

---
+++ bars.js
@@ -0,0 +1,135 @@
+const cvs1 = document.getElementById("1");
+const ctx1 = cvs1.getContext("2d");
+const cvs2 = document.getElementById("2");
+const ctx2 = cvs2.getContext("2d");
+const cvs3 = document.getElementById("3");
+const ctx3 = cvs3.getContext("2d");
+const cvs4 = document.getElementById("4");
+const ctx4 = cvs4.getContext("2d");
+const cvs6 = document.getElementById("6");
+const ctx6 = cvs6.getContext("2d");
+const cvs7 = document.getElementById("7");
+const ctx7 = cvs7.getContext("2d");
+const cvs8 = document.getElementById("8");
+const ctx8 = cvs8.getContext("2d");
+const cvs9 = document.getElementById("9");
+const ctx9 = cvs9.getContext("2d");
+const cvs10 = document.getElementById("10");
+const ctx10 = cvs10.getContext("2d");
+const cvs11 = document.getElementById("11");
+const ctx11 = cvs11.getContext("2d");
+const cvs12 = document.getElementById("12");
+const ctx12 = cvs12.getContext("2d");
+
+set1 = false;
+set2 = true;
+set3 = false;
+set4 = false;
+set6 = true;
+set7 = false;
+set8 = false;
+set9 = false;
+set10 = false;
+set11 = false;
+set12 = false;
+set13 = false;
+set14 = false;
+
+var purple = "#550099";
+var grey = "#707070";
+
+function showBars(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+
+    if ((x <= 160 && (y > 85 && y < 120)) || set1) {
+        ctx1.fillStyle = purple;
+        ctx1.fillRect(0, 0, 8, 35);
+    }
+    else {
+    ctx1.fillStyle = grey;
+        ctx1.fillRect(0, 0, 8, 35);
+    }
+    if ((x <= 160 && (y > 135 && y < 170)) || set2) {
+        ctx2.fillStyle = purple;
+        ctx2.fillRect(0, 0, 8, 35);
+    }
+    else {
+        ctx2.fillStyle = grey;
+        ctx2.fillRect(0, 0, 8, 35);
+    }
+    if ((x <= 160 && (y > 185 && y < 220)) || set3) {
+        ctx3.fillStyle = purple;
+        ctx3.fillRect(0, 0, 8, 35);
+    }
+    else {
+        ctx3.fillStyle = grey;
+        ctx3.fillRect(0, 0, 8, 35);
+    }
+    if ((x <= 160 && (y > 235 && y < 270)) || set4) {
+        ctx4.fillStyle = purple;
+        ctx4.fillRect(0, 0, 8, 35);
+    }
+    else {
+        ctx4.fillStyle = grey;
+        ctx4.fillRect(0, 0, 8, 35);
+    }
+    if ((x <= 160 && (y > 285 && y < 320)) || set6) {
+        ctx6.fillStyle = purple;
+        ctx6.fillRect(0, 0, 8, 35);
+    }
+    else {
+        ctx6.fillStyle = grey;
+        ctx6.fillRect(0, 0, 8, 35);
+    }
+    if ((x <= 160 && (y > 335 && y < 370)) || set7) {
+        ctx7.fillStyle = purple;
+        ctx7.fillRect(0, 0, 8, 35);
+    }
+    else {
+        ctx7.fillStyle = grey;
+        ctx7.fillRect(0, 0, 8, 35);
+    }
+    if ((x <= 160 && (y > 385 && y < 420)) || set8) {
+        ctx8.fillStyle = purple;
+        ctx8.fillRect(0, 0, 8, 35);
+    }
+    else {
+        ctx8.fillStyle = grey;
+        ctx8.fillRect(0, 0, 8, 35);
+    }
+    if ((x <= 160 && (y > 435 && y < 470)) || set9) {
+        ctx9.fillStyle = purple;
+        ctx9.fillRect(0, 0, 8, 35);
+    }
+    else {
+        ctx9.fillStyle = grey;
+        ctx9.fillRect(0, 0, 8, 35);
+    }
+    if ((x <= 160 && (y > 485 && y < 520)) || set10) {
+        ctx10.fillStyle = purple;
+        ctx10.fillRect(0, 0, 8, 35);
+    }
+    else {
+        ctx10.fillStyle = grey;
+        ctx10.fillRect(0, 0, 8, 35);
+    }
+    if ((x <= 160 && (y > 535 && y < 570)) || set11) {
+        ctx11.fillStyle = purple;
+        ctx11.fillRect(0, 0, 8, 35);
+    }
+    else {
+        ctx11.fillStyle = grey;
+        ctx11.fillRect(0, 0, 8, 35);
+    }
+    if ((x <= 160 && (y > 585 && y < 620)) || set12) {
+        ctx12.fillStyle = purple;
+        ctx12.fillRect(0, 0, 8, 35);
+    }
+    else {
+        ctx12.fillStyle = grey;
+        ctx12.fillRect(0, 0, 8, 35);
+    }
+}
+
+window.addEventListener("mousemove", showBars);
---
+++ whiteboard.js
@@ -0,0 +1,145 @@
+const cvs = document.getElementById("whiteboard");
+const ctx = cvs.getContext("2d");
+
+cvs.height = window.innerHeight - 0;
+cvs.width = window.innerWidth - 160;
+
+function drawRect(x, y, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+}
+
+var thick = 12;
+var medium = 6;
+var thin = 2;
+
+var black = "#000000";
+var red = "#ff2222";
+var orange = "#ff8c00";
+var yellow = "#ffd700";
+var green = "#118111";
+var blue = "#1e90ff";
+var purple = "#550099";
+
+function clear() {
+    drawRect(0, 0, cvs.width, cvs.height, "white")
+}
+
+clear();
+
+const pen = {
+    x : 0,
+    y : 0,
+    size : medium,
+    color : black
+}
+
+var recentColor = pen.color;
+var recentSize = pen.size;
+
+document.getElementById("thick").onclick = function() {changePen(thick, recentColor); set1 = true; set2 = false; set3 = false;};
+document.getElementById("medium").onclick = function() {changePen(medium, recentColor); set1 = false; set2 = true; set3 = false;};
+document.getElementById("thin").onclick = function() {changePen(thin, recentColor); set1 = false; set2 = false; set3 = true;};
+
+document.getElementById("erasor").onclick = function() {changePen(thick, "white")};
+
+document.getElementById("black").onclick = function() {changePen(recentSize, black); set6 = true; set7 = false; set8 = false; set9 = false; set10 = false; set11 = false; set12 = false;};
+document.getElementById("red").onclick = function() {changePen(recentSize, red); set6 = false; set7 = true; set8 = false; set9 = false; set10 = false; set11 = false; set12 = false;};
+document.getElementById("orange").onclick = function() {changePen(recentSize, orange); set6 = false; set7 = false; set8 = true; set9 = false; set10 = false; set11 = false; set12 = false;};
+document.getElementById("yellow").onclick = function() {changePen(recentSize, yellow); set6 = false; set7 = false; set8 = false; set9 = true; set10 = false; set11 = false; set12 = false;};
+document.getElementById("green").onclick = function() {changePen(recentSize, green); set6 = false; set7 = false; set8 = false; set9 = false; set10 = true; set11 = false; set12 = false;};
+document.getElementById("blue").onclick = function() {changePen(recentSize, blue); set6 = false; set7 = false; set8 = false; set9 = false; set10 = false; set11 = true; set12 = false;};
+document.getElementById("purple").onclick = function() {changePen(recentSize, purple); set6 = false; set7 = false; set8 = false; set9 = false; set10 = false; set11 = false; set12 = true;};
+
+document.getElementById("clear").onclick = function() {clear()};
+
+function changePen(size, color) {
+    // if changing to an erasor, save the size and color before the change
+    if (color == "white") {
+        recentColor = pen.color;
+        recentSize = pen.size;
+        set4 = true;
+
+        set1 = false;
+        set2 = false;
+        set3 = false;
+        set6 = false;
+        set7 = false;
+        set8 = false;
+        set9 = false;
+        set10 = false;
+        set11 = false;
+        set12 = false;
+        set13 = false;
+        set14 = false;
+    }
+    // if not changing to an erasor, update the size and color to be the values after the change
+    else {
+        if (recentColor == color) {
+            if (color == black) {
+                set6 = true; set7 = false; set8 = false; set9 = false; set10 = false; set11 = false; set12 == false;
+            }
+            else if (color == red) {
+                set6 = false; set7 = true; set8 = false; set9 = false; set10 = false; set11 = false; set12 == false;
+            }
+            else if (color == orange) {
+                set6 = false; set7 = false; set8 = true; set9 = false; set10 = false; set11 = false; set12 == false;
+            }
+            else if (color == yellow) {
+                set6 = false; set7 = false; set8 = false; set9 = true; set10 = false; set11 = false; set12 == false;
+            }
+            else if (color == green) {
+                set6 = false; set7 = false; set8 = false; set9 = false; set10 = true; set11 = false; set12 == false;
+            }
+            else if (color == blue) {
+                set6 = false; set7 = false; set8 = false; set9 = false; set10 = false; set11 = true; set12 == false;
+            }
+            else if (color == purple) {
+                set6 = false; set7 = false; set8 = false; set9 = false; set10 = false; set11 = false; set12 == true;
+            }
+        }
+        else if (recentSize == size) {
+            if (size == thick) {
+                set1 = true; set2 = false; set3 = false;
+            }
+            else if (size == medium) {
+                set1 = false; set2 = true; set3 = false;
+            }
+            else if (size == thin) {
+                set1 = false; set2 = false; set3 = true;
+            }
+        }
+
+        recentColor = color;
+        recentSize = size;
+        set4 = false;
+    }
+
+    pen.size = size;
+    pen.color = color;
+}
+
+var drawing = false;
+var dotting = false;
+
+cvs.addEventListener("mousedown", (event) => {
+    drawing = true;
+    update(event);
+});
+
+cvs.addEventListener("mouseup", () => {
+    drawing = false;
+    ctx.beginPath();
+});
+
+function update(event) {
+    if (drawing) {
+        ctx.lineWidth = pen.size;
+        ctx.lineCap = "round";
+        ctx.lineTo(event.clientX - 160, event.clientY);
+        ctx.strokeStyle = pen.color;
+        ctx.stroke();
+    }
+}
+
+cvs.addEventListener("mousemove", update);
